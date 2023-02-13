ca603fabf5c94b8c92a0d0923009539693627dc6
SrijanSShetty <srijansshetty@gmail.com>
2023-02-13 23:37:29 +0530

    done

---
+++ Regular assignments/Week 2/Day 1/DisplayDetails.java
@@ -0,0 +1,8 @@
+class DisplayDetails {
+  public static void main(String args[]){
+    System.out.println("This is Srijan S Shetty ");
+    System.out.println("From PA College of Engineering");
+    System.out.println("Under Computer Science & Engineering");
+      System.out.println("He belongs to B section ");
+ }
+}
